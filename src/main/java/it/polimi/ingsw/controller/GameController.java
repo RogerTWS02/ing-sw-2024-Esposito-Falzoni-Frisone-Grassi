2c6ec8fe51b28eeba8e59c461092eaa3fec689f6
Elia <elia.falzoni@mail.polimi.it>
2024-03-28 00:21:34 +0100

    drawCard implemented in GameController

--- src/main/java/it/polimi/ingsw/controller/GameController.java
+++ src/main/java/it/polimi/ingsw/controller/GameController.java
@@ -1,5 +1,20 @@
 package it.polimi.ingsw.controller;
 
+import java.util.Random;
+
 public class GameController {
-    private int numberOfPlayers;
+
+
+    //Draws a card from the deck passed by parameter
+    public Object drawCard(Object[] deck){
+        int lastCardIndex;
+        for(lastCardIndex = deck.length - 1; deck[lastCardIndex] == null; lastCardIndex--);
+        int bound = lastCardIndex + 1;
+        Random random = new Random();
+        int randomIndex = random.nextInt(bound);
+        Object card = deck[randomIndex];
+        deck[randomIndex] = deck[lastCardIndex];
+        deck[lastCardIndex] = null;
+        return card;
+    }
 }
