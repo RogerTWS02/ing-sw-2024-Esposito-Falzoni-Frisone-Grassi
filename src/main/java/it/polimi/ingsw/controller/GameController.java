b7041145989b32110db173936938ff0db528e64b
Spiderman29 <francesco2.grassi@mail.polimi.it>
2024-04-02 16:14:11 +0200

    Added drawFromViewable method

--- src/main/java/it/polimi/ingsw/controller/GameController.java
+++ src/main/java/it/polimi/ingsw/controller/GameController.java
@@ -16,7 +16,7 @@ public class GameController {
     }
 
     //Draws a card from the deck passed by parameter
-    public Object drawCard(JSONArray deck){
+    public Object drawFromDeck(JSONArray deck){
         Random random = new Random();
         int randomIndex = random.nextInt(deck.size());
         JSONObject JSONcard = (JSONObject) deck.get(randomIndex);
@@ -25,6 +25,34 @@ public class GameController {
         //TODO: parse the JSON object and craft the card
 
         return null; //So IDE doesn't give error
+    }
+
+    /**
+     * This method draws a card from the ones which are positioned on the table with their faces uncovered,
+     * and it substitutes the viewable card picked up with a new card from the deck
+     *
+     * @param i which deck to choose from
+     * @param chosen which card the player wants to pick up between the 2 available
+     * @return the card
+     */
+
+    //We might have to add a method getDeck in Game
+    public PlayableCard drawFromViewable(boolean i, int chosen){
+        PlayableCard card;
+        if(i){
+            //draw from resource cards
+            card = currentGame.viewableResourceCards[chosen];
+            ResourceCard[] newviewable = currentGame.getViewableResourceCards();
+            newviewable[chosen] = drawFromDeck(resourceDeck);
+            currentGame.setViewableResourceCards(newviewable);
+        }else{
+            //draw from golden cards
+            card = currentGame.viewableGoldenCards[chosen];
+            GoldenCard[] newviewable = currentGame.getViewableGoldenCards();
+            newviewable[chosen] = drawFromDeck(goldenDeck);
+            currentGame.setViewableGoldenCards(newviewable);
+        }
+        return card;
     }
 
     /* not very clear how we can add a new player if the array is private and there's no setPlayer function,
@@ -70,10 +98,10 @@ public class GameController {
      */
     public List<int[]> showAvailableOnBoard(Player player){
     List<int []> availablePosition = new ArrayList<>();
-    int [] possiblePosition= new int[2];
+    int[] possiblePosition = new int[2];
         for (int i = 0; i <= 80; i++) {
             for (int j = 0; j <= 80; j++) {
-                if (player.getPlayerBoard().getState(i,j)== State.AVAILABLE) {
+                if (player.getPlayerBoard().getState(i,j) == State.AVAILABLE) {
                     possiblePosition[0] = i;
                     possiblePosition[1] = j;
                     availablePosition.add(possiblePosition);
@@ -86,10 +114,8 @@ public class GameController {
  // needs some more detail, really useful?
     public PlayableCard[] showViewable(boolean type){
         if(type){return currentGame.getViewableGoldenCards();
-
         }
         else{ return currentGame.getViewableResourceCards();
-
         }
     }
 
@@ -100,10 +126,10 @@ public class GameController {
     public void getPointsFromGoalCards(Player player){
         // firstly common goal
         for (GoalCard gc: currentGame.getCommonGoalCards()){
-            player.setScore(player.getScore()+ gc.checkGoal(player.getPlayerBoard()));
+            player.setScore(player.getScore() + gc.checkGoal(player.getPlayerBoard()));
         }
         //secondly secret goal
-        player.setScore(player.getScore()+ player.getSecretGoalCard().checkGoal(player.getPlayerBoard()));
+        player.setScore(player.getScore() + player.getSecretGoalCard().checkGoal(player.getPlayerBoard()));
     }
 
 
