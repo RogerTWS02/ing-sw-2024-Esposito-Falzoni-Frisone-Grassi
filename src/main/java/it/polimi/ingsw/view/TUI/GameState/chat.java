5024f205beae102aaac4e666f1ba1b649715e4f8
Spiderman29 <francesco2.grassi@mail.polimi.it>
2024-05-09 16:11:11 +0200

    Added chat graphic method

--- src/main/java/it/polimi/ingsw/view/TUI/GameState/chat.java
+++ src/main/java/it/polimi/ingsw/view/TUI/GameState/chat.java
@@ -3,12 +3,42 @@ package it.polimi.ingsw.view.TUI.GameState;
 public class chat implements Views{
 
         public static void main(String[] args) {
-
+            StringBuilder sb = new StringBuilder();
+            sb
+                    .append("┌")
+                    .append("─".repeat(106))
+                    .append("┐")
+                    .append("\n")
+                    .append("│")
+                    .append(" ".repeat(51))
+                    .append("CHAT")
+                    .append(" ".repeat(51))
+                    .append("│")
+                    .append("\n")
+                    .append("├")
+                    .append("─".repeat(106))
+                    .append("┤")
+                    .append("\n");
+            for(int line = 25; line >0; line --){
+                sb
+                        .append("│")
+                        .append(" ".repeat(106))
+                        .append("│")
+                        .append("\n");
+            }
+            sb
+                    .append("└")
+                    .append("─".repeat(106))
+                    .append("┘");
+            System.out.println(sb);
         }
 public void showChat(String[] msg){
 
                 //clear the screen before printing something
                 Views.clearScreen();
+
+
+                //Lorenzo Method
                 System.out.println("┌──────────────────────────────────────────────────────────────────────────────────────────────────────────┐");
                 System.out.println("│                                                   CHAT                                                   │");
                 System.out.println("├──────────────────────────────────────────────────────────────────────────────────────────────────────────┤");
